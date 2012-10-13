f222f60563dfcfd19beb160e70992a806ec61526
Edward Aleksander Pierzchalski <e.a.pierzchalski@gmail.com>
2012-10-13 15:10:09 +1100

    LOTS OF CRAP

--- src/Quoridor/Board.java
+++ src/Quoridor/Board.java
@@ -4,12 +4,22 @@ package Quoridor;
  * Created with IntelliJ IDEA.
  * User: Pierzchalski
  * Date: 13/10/12
- * Time: 2:33 PM
+ * Time: 2:38 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Board {
+public interface Board {
+    /**
+     *
+     * @return a copy of the board
+     */
+    //Board getCopy();
 
-    public String toString() {
-        return null;
-    }
+    //void movePiece(GamePiece piece, int move);
+
+    //void validateMove(int move);
+
+    void addPiece(GamePiece piece, int row, int col);
+
+    public void print();
+
 }
---
+++ src/Quoridor/GamePiece.java
@@ -0,0 +1,11 @@
+package Quoridor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pierzchalski
+ * Date: 13/10/12
+ * Time: 2:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface GamePiece {
+}
