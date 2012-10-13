bde2a840452215ce8dfb4058bdddb7ed830cd468
Edward Aleksander Pierzchalski <e.a.pierzchalski@gmail.com>
2012-10-13 14:35:15 +1100

    Put the validator tests in their own package

---
+++ src/Quoridor/Board.java
@@ -0,0 +1,15 @@
+package Quoridor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pierzchalski
+ * Date: 13/10/12
+ * Time: 2:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Board {
+
+    public String toString() {
+        return null;
+    }
+}
