f2f6c1361c6c1241a0654c91c82c1b77211ff8d7
Edward Aleksander Pierzchalski <e.a.pierzchalski@gmail.com>
2012-10-13 14:25:15 +1100

    ed test

---
+++ src/Quoridor/test.java
@@ -0,0 +1,11 @@
+package Quoridor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pierzchalski
+ * Date: 13/10/12
+ * Time: 2:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}                blah
