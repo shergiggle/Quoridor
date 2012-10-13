187565b77ef9ce56cb96e273770085d02cebbe55
Edward Aleksander Pierzchalski <e.a.pierzchalski@gmail.com>
2012-10-13 14:28:43 +1100

    Ed's second test

--- src/Quoridor/TEST2BIUATCHEUS.java
+++ src/Quoridor/TEST2BIUATCHEUS.java
@@ -4,8 +4,9 @@ package Quoridor;
  * Created with IntelliJ IDEA.
  * User: Pierzchalski
  * Date: 13/10/12
- * Time: 2:22 PM
+ * Time: 2:26 PM
  * To change this template use File | Settings | File Templates.
  */
-public class test {
-}                blah
+public class TEST2BIUATCHEUS {
+    public static String ollo;
+}
