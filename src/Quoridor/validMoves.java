10221f5c734e8c443d0cc55c9beb90f3798741b7
shergiggle <sheshy_2004@hotmail.com>
2012-10-13 13:51:16 +1100

    starting Project

---
+++ src/Quoridor/validMoves.java
@@ -0,0 +1,17 @@
+package Quoridor;
+
+public class validMoves implements Display{
+
+	@Override
+	public void display(String moves) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validMoves(String moves) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
