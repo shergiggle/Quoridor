f7d7a71979e81256c36a21516b858bc7fe920def
Edward Aleksander Pierzchalski <e.a.pierzchalski@gmail.com>
2012-10-14 16:17:25 +1100

    made a test class to see if git is not being a git

---
+++ src/Quoridor/TestClass.java
@@ -0,0 +1,11 @@
+package Quoridor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pierzchalski
+ * Date: 14/10/12
+ * Time: 4:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+}
